ded6fb54a3852262626b224dcff3bb6867917744
daijian <daijian24@icloud.com>
2017-09-20 22:03:20 +0800

    add new_topic

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 from .models import Topic
+from .forms import TopicForm
+
 # Create your views here.
 
 def index(request):
@@ -17,3 +21,16 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries':entries}
     return render(request, 'learning_logs/topic.html',context)
+
+def new_topic(request):
+    """添加新的主题"""
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
